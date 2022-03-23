be92b6a8bf1963afd2a8becf12ea16f29f56250d
“SSONNGG” <1647478489@qq.com>
2022-03-23 21:13:45 +0800

    Commit EARLY Development content include: Add MyFragment Pages User Part UI design.

--- app/src/main/java/com/song/worterdemo/fragment/MyFragment.java
+++ app/src/main/java/com/song/worterdemo/fragment/MyFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideOption;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.song.worterdemo.R;
 
 //我的
@@ -36,6 +41,21 @@ public class MyFragment extends Fragment {
         if(rootView==null){
             rootView= inflater.inflate(R.layout.fragment_my, container, false);
         }
+
+        //创建请求配置，配置占位符
+        RequestOptions requestOptions=new RequestOptions()
+                .placeholder(R.drawable.pic_user)
+                .error(R.drawable.pic_user)
+                .fallback(R.drawable.pic_user)
+                .override(80,80);//强制指定加载图片的大小
+
+
+        ImageView iv=rootView.findViewById(R.id.iv_pic);
+        Glide.with(getActivity())
+                .load("https://s2.loli.net/2022/03/23/w3ShNOnkuDL59of.jpg")
+                .apply(requestOptions)
+                .transform(new CircleCrop())
+                .into(iv);
         return rootView;
     }
 }
